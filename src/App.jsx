a29874f23cfb606bff74d453cbaf801f27d7d0c2
akbarsiddic <rizkyakbar.rpl22020@smkn4bogor.sch.id>
2023-08-29 13:13:52 +0700

    add route

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Register from './Pages/Register'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
+import LandingPage from "./Pages/LandingPage/LandingPage";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#FABC1D"
+      main: "#FABC1D",
     },
     secondary: {
-      main: "#5B4947"
-    }
-  }
+      main: "#5B4947",
+    },
+  },
 });
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div>
+      <Router>
         <Navbar />
-        <Register />
-      </div>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
