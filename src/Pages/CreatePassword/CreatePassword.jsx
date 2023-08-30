ffbcec69d03225d4c3f720f83cb020abb777cd4d
akbarsiddic <rizkyakbar.rpl22020@smkn4bogor.sch.id>
2023-08-30 12:24:49 +0700

    useState

--- src/Pages/CreatePassword/CreatePassword.jsx
+++ src/Pages/CreatePassword/CreatePassword.jsx
@@ -1,53 +1,60 @@
 import React from "react";
 import "./createpassword.css";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Input from "../../components/Input";
 import { Button } from "@mui/material";
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#FABC1D",
-    },
-    secondary: {
-      main: "#5B4947",
-    },
-  },
-});
+import { useState } from "react";
 
 function CreatePassword() {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const inputOnchange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  // on submit form console.log(data)
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(data);
+  };
   return (
-    <ThemeProvider theme={theme}>
-      <div>
-        <div className="formDiv">
-          <form action="">
-            <h1>Create Password</h1>
-            <p></p>
-            <div className="inputDiv">
-              <Input labelInput="Email" inputName="email" inputType="email" />
-              <Input
-                labelInput="Password"
-                inputName="password"
-                inputType="password"
-              />
-            </div>
-            <div className="buttonDiv">
-              <Button
-                variant="outlined"
-                type="submit"
-                color="secondary"
-                sx={{ py: 1, px: 6 }}
-              >
-                Submit
-              </Button>
-              <Button variant="contained" type="submit" sx={{ py: 1, px: 6 }}>
-                Login
-              </Button>
-            </div>
-          </form>
-        </div>
+    <div>
+      <div className="formDiv">
+        <form action="" onSubmit={onSubmit}>
+          <h1>Create Password</h1>
+          <p></p>
+          <div className="inputDiv">
+            <Input
+              labelInput="Email"
+              inputName="email"
+              inputType="email"
+              inputOnchange={inputOnchange}
+            />
+            <Input
+              inputOnchange={inputOnchange}
+              labelInput="Password"
+              inputName="password"
+              inputType="password"
+            />
+          </div>
+          <div className="buttonDiv">
+            <Button
+              variant="outlined"
+              type="submit"
+              color="secondary"
+              sx={{ py: 1, px: 6 }}
+            >
+              Submit
+            </Button>
+            <Button variant="contained" type="submit" sx={{ py: 1, px: 6 }}>
+              Login
+            </Button>
+          </div>
+        </form>
       </div>
-    </ThemeProvider>
+    </div>
   );
 }
 
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,23 +2,44 @@ import React from "react";
 import "./login.css";
 import Input from "../../components/Input";
 import { Button } from "@mui/material";
+import { useState } from "react";
 
 function Login() {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const inputOnchange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  // on submit form console.log(data)
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(data);
+  };
   return (
     <div>
       <div className="formDiv">
-        <form action="">
+        <form action="" onSubmit={onSubmit}>
           <h1>Welcome Back! Cheff</h1>
           <p>Please login first</p>
           <div className="inputDiv">
-            <Input labelInput="Email" inputName="email" inputType="email" />
+            <Input
+              labelInput="Email"
+              inputName="email"
+              inputType="email"
+              inputOnchange={inputOnchange}
+            />
             <Input
               labelInput="Password"
+              inputOnchange={inputOnchange}
               inputName="password"
               inputType="password"
             />
             <p>
-              Forgot Password? <a href="/createpassword">Click here</a>{" "}
+              Forgot Password? <a href="/reset">Click here</a>{" "}
             </p>
           </div>
           <Button variant="contained" type="submit" sx={{ py: 1, px: 6 }}>
