b5932684e1d20ab53b783b9bb9ee5d12ed1e8ade
Angga Dharma <anggadharmaps@gmail.com>
2023-08-30 12:20:16 +0700

    add-createpassword

--- src/Pages/CreatePassword/CreatePassword.jsx
+++ src/Pages/CreatePassword/CreatePassword.jsx
@@ -1,53 +1,25 @@
 import React from "react";
 import "./createpassword.css";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Input from "../../components/Input";
 import { Button } from "@mui/material";
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#FABC1D",
-    },
-    secondary: {
-      main: "#5B4947",
-    },
-  },
-});
-
 function CreatePassword() {
   return (
-    <ThemeProvider theme={theme}>
       <div>
         <div className="formDiv">
           <form action="">
             <h1>Create Password</h1>
-            <p></p>
             <div className="inputDiv">
-              <Input labelInput="Email" inputName="email" inputType="email" />
-              <Input
-                labelInput="Password"
-                inputName="password"
-                inputType="password"
-              />
+              <Input labelInput="New Password" inputName="password" inputType="password" />
+              <Input labelInput="Password" inputName="password" inputType="password" />
             </div>
             <div className="buttonDiv">
-              <Button
-                variant="outlined"
-                type="submit"
-                color="secondary"
-                sx={{ py: 1, px: 6 }}
-              >
-                Submit
-              </Button>
-              <Button variant="contained" type="submit" sx={{ py: 1, px: 6 }}>
-                Login
-              </Button>
+              <Button href="/reset" variant="outlined" type="cancel" color="secondary" sx={{ py: 1, px: 6 }}>Cancel</Button>
+              <Button href="/" variant="contained" type="submit" sx={{ py: 1, px: 6 }} >Submit</Button>
             </div>
           </form>
         </div>
       </div>
-    </ThemeProvider>
   );
 }
 
