4e9cfc3c5de2f34111e46a8119e52546dfd7fa86
Angga Dharma <anggadharmaps@gmail.com>
2023-09-01 22:54:52 +0700

    edit-menuclass

--- src/Pages/MenuClass/MenuClass.jsx
+++ src/Pages/MenuClass/MenuClass.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
 import "./menuclass.css";
-import { Grid } from "@mui/material";
+import { Grid } from '@mui/material';
 import Card from "../../components/MenuCard";
 import axios from "axios";
 // import sushi from "./images/sushi.png"
@@ -101,7 +101,7 @@ const MenuClass = () => {
     }
 
     useEffect(() => {
-        getDataClass();
+        getDataClass(id);
         getDataMenu();
     }, []);
 
@@ -111,7 +111,7 @@ const MenuClass = () => {
         <div>
             <section className="menu-class">
                 <div className="header-bg">
-                    <img src="./images/asian.png" alt="Header" />
+                    <img src="/images/asian.png" alt="Header" />
                 </div>
             </section>
             <section className="description">
@@ -138,32 +138,8 @@ const MenuClass = () => {
                 </Grid>
             </section>
         </div>
-      </section>
-      <section className="description">
-        <h1>{data.type_name}</h1>
-        <p>{data.description}</p>
-      </section>
-      <section className="menu">
-        <div>
-          <h1>Another menu in this class</h1>
-        </div>
-        <Grid container spacing={2}>
-          {menuItems.map((item) => {
-            return (
-              <Grid item xs={13} sm={6} md={4} key={item.id}>
-                <Card
-                  category={item.category}
-                  name={item.name}
-                  price={item.price}
-                  image={item.image}
-                />
-              </Grid>
-            );
-          })}
-        </Grid>
-      </section>
-    </div>
-  );
-};
+
+    )
+}
 
 export default MenuClass;
