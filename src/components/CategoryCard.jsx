ac41de1cd17539cf72a3e8a61d0c5ef7f781cda6
akbarsiddic <rizkyakbar.rpl22020@smkn4bogor.sch.id>
2023-09-01 17:32:02 +0700

    navigasi category card

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -2,21 +2,24 @@ import * as React from "react";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
 
 export default function MediaCard({ name, image }) {
   return (
     <Card sx={{ maxWidth: 200 }}>
-      <CardMedia sx={{ height: 140 }} image={image} title={name} />
-      <Typography
-        gutterBottom
-        variant="h5"
-        sx={{
-          marginTop: 2,
-          textAlign: "center",
-        }}
-      >
-        {name}
-      </Typography>
+      <CardActionArea href="/menuclass">
+        <CardMedia sx={{ height: 140 }} image={image} title={name} />
+        <Typography
+          gutterBottom
+          variant="h5"
+          sx={{
+            marginTop: 2,
+            textAlign: "center",
+          }}
+        >
+          {name}
+        </Typography>
+      </CardActionArea>
     </Card>
   );
 }
