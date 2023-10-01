e51426c3c263816eaa72b6efee63fef050615994
akbarsiddic <rizkyakbar.rpl22020@smkn4bogor.sch.id>
2023-10-01 09:17:46 +0700

    fix navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./component.css";
 import { Button } from "@mui/material";
 import { Outlet, Link, useNavigate } from "react-router-dom";
+import Drawer from "@mui/material/Drawer";
 import logo from "./logo.png";
 import useAuth from "../../hooks/useAuth";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import PersonIcon from "@mui/icons-material/Person";
 import LogoutIcon from "@mui/icons-material/Logout";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function Navbar() {
   const { isLoggedIn, logout } = useAuth();
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
   const navigate = useNavigate();
 
   const handleLogout = () => {
@@ -17,14 +21,96 @@ function Navbar() {
     navigate("/");
   };
 
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+  const cartClick = () => {
+    navigate("/cart");
+    toggleDrawer();
+  };
+
   return (
     <div>
       <nav>
-        <div>
+        <div className="logo-button">
           <Button href="/">
             <img src={logo} alt="Logo" />
           </Button>
         </div>
+
+        <Drawer anchor="left" open={isDrawerOpen} onClose={toggleDrawer}>
+          <div className="drawer-content">
+            <ul>
+              {isLoggedIn ? (
+                <>
+                  <li>
+                    <Button variant="contained" sx={{ width: 120 }}>
+                      <Link
+                        reloadDocument
+                        to={`/myclass`}
+                        onClick={toggleDrawer}
+                        style={{ textDecoration: "none", color: "black" }}
+                      >
+                        My Class
+                      </Link>
+                    </Button>
+                  </li>
+                  <li>
+                    <Button variant="contained" sx={{ width: 120 }}>
+                      <Link
+                        reloadDocument
+                        to={`/invoice`}
+                        onClick={toggleDrawer}
+                        style={{ textDecoration: "none", color: "black" }}
+                      >
+                        Invoice
+                      </Link>
+                    </Button>
+                  </li>
+                  <li>
+                    <ShoppingCartIcon
+                      onClick={cartClick}
+                      color="secondary"
+                      cursor="pointer"
+                    />
+                  </li>
+                  <li onClick={toggleDrawer}>
+                    <PersonIcon color="primary" />
+                  </li>
+                  <li onClick={handleLogout}>
+                    <LogoutIcon cursor="pointer" color="secondary" />
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li>
+                    <Button
+                      variant="outlined"
+                      sx={{ width: "100%" }}
+                      color="secondary"
+                      href="/register"
+                      onClick={toggleDrawer}
+                    >
+                      Register
+                    </Button>
+                  </li>
+                  <li>
+                    <Button
+                      href="/login"
+                      variant="contained"
+                      sx={{ width: "100%" }}
+                      onClick={toggleDrawer}
+                    >
+                      Login
+                    </Button>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
+        </Drawer>
+        {/* Render the button to open Drawer */}
+
         <div className="button-container">
           {isLoggedIn ? (
             <>
@@ -70,6 +156,12 @@ function Navbar() {
               </Button>
             </>
           )}
+        </div>
+        <div className="menu-icon">
+          {/* Add a button to toggle the Drawer */}
+          <Button onClick={toggleDrawer}>
+            <MenuIcon />
+          </Button>
         </div>
       </nav>
       <Outlet />
