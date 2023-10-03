5cfa470fe153ef74980e4dc42f48eed9a03c66df
Angga Dharma <anggadharmaps@gmail.com>
2023-10-03 12:54:57 +0700

    update 03 oct 2023

--- src/components/AccessDenied.jsx
+++ src/components/AccessDenied.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import CancelPresentationIcon from '@mui/icons-material/CancelPresentation';
 
 const AccessDenied = () => {
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+  };
+
+  const iconStyle = {
+    fontSize: 100,
+    color: 'red',
+    marginBottom: 20,
+  };
+
   return (
-    <div>
-      <h2>Access Denied</h2>
-      <p>Anda tidak memiliki izin untuk mengakses halaman admin.</p>
-      {/* Anda juga dapat menambahkan tautan untuk kembali ke halaman utama atau login */}
-    </div>
+    <Grid container style={containerStyle}>
+      <Grid item>
+        <CancelPresentationIcon style={iconStyle} />
+      </Grid>
+      <Grid item>
+        <Typography variant="h4" gutterBottom>
+          Access Denied
+        </Typography>
+        <Typography variant="body1" paragraph>
+          Anda tidak memiliki izin untuk mengakses halaman admin.
+        </Typography>
+        <Button variant="contained" color="primary" href="/">
+          Kembali ke Halaman Utama
+        </Button>
+      </Grid>
+    </Grid>
   );
 };
 
